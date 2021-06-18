57bffbf87f245d2b1fb633ff829929c0a04bfdb9
LOL <pmoncrnorth@ppa.com.ph>
2021-06-18 11:20:32 +0800

    fix

--- push.js
+++ push.js
@@ -9,6 +9,15 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {};
+let sub = {
+    endpoint:
+    'https://fcm.googleapis.com/fcm/send/fTtASD-YRp0:APA91bHIwJLK6ki6KbAQ3nus3SaiAllD0g_ltgbXWHII0c2LMEQh-sL9qUeJsHuuP_nyS2A2tdURD6CCeENYXxFLE_S8LCFLSeeT_4GfJKnVcRc_BegXT6ne1WT-Z1wqPH7oQD3XFzb1',
+expirationTime:null,
+keys:  {
+p256dh:'BDGb_cNcm-udvjzHq7Nw3o7YYRn2pBRbEqvbsuB-3U9T0uqDs0BgXqppYRFUsu360AAvg2bwS8UG7fOm-Q5lTVU',
+auth: 'vRC_uKlwk9y6jfu_p_cySA'
+
+}
+};
 
 push.sendNotification(sub, 'test message')
--- sw.js
+++ sw.js
@@ -1,4 +1,22 @@
-self.addEventListener('push', () =>{
-set.registration.sendNotification('test message', {});
-
-});
+self.addEventListener('push', function (e)  {
+    var options ={
+      body: 'PPA MONITORING APP',
+      //icon: 'images/example.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2'
+      },
+      actions: [
+        {
+          action: 'explore',
+          title: 'PLEASE SUBMIT YOUR REPORT ASAP!',
+          //icon: 'images/checkmark.png'
+        },
+        { action: 'close', title: 'Close'}
+       ]
+      
+    };
+    e.waitUntil(self.registration.showNotification('PLEASE SUBMIT YOUR REPORT', options));
+ });
+ 
